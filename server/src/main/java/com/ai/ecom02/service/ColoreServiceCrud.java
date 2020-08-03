ca15efb1402d03908011c81149e2299b6826ae4d
DESKTOP-323JE3F\Gian Marco <g.petrillo@accademiainformatica.com>
2020-08-03 17:58:30 +0200

    Modificato il service, ora non utilizza più generics.

---
+++ server/src/main/java/com/ai/ecom02/service/ColoreServiceCrud.java
@@ -0,0 +1,17 @@
+package com.ai.ecom02.service;
+
+import com.ai.ecom02.model.Colore;
+import java.util.List;
+public interface ColoreServiceCrud {
+
+    public void add(Colore colore);
+
+    public void delete(Colore colore);
+
+    public void update(Colore colore);
+
+    public List<Colore> getAll();
+    
+    public Colore findById(Colore colore);
+    
+}
