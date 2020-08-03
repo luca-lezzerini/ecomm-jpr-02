744fa3991e1dfe1825c977db1c39817131833a85
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2020-08-03 14:37:14 +0200

    Risolta issue Creare progetto base #16

---
+++ server/src/test/java/com/ai/ecom02/Ecom02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.ecom02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecom02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
