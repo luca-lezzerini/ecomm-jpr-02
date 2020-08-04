6ce7dc4bcbbbd91a9521438608b7185b92ea5614
Eugenio <e.corio@accademiainformatica.com>
2020-08-04 14:49:27 +0200

    Formatto "RepColore.java"

--- server/src/main/java/com/ai/ecom02/repository/RepColore.java
+++ server/src/main/java/com/ai/ecom02/repository/RepColore.java
@@ -10,8 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepColore extends JpaRepository <Colore, Long>{
-    
-       
+public interface RepColore extends JpaRepository<Colore, Long> {
     public Colore findByColore(String colore);
 }
