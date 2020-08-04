d6e4e5b3671140ebf43650decf196d98ab75ae00
David-Michele <67779702+David-Michele@users.noreply.github.com>
2020-08-04 18:50:48 +0200

    Component colore crud
    Compilabile ma non funzionante

--- client-ecomm02/src/app/colore-crud/colore-crud.component.ts
+++ client-ecomm02/src/app/colore-crud/colore-crud.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ColoreDto } from '../dto/colore-dto';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-colore-crud',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colore-crud.component.css']
 })
 export class ColoreCrudComponent implements OnInit {
+  urlPath = 'http://localhost:8080';
+colore: ColoreDto = new ColoreDto();
+coloreForm: ColoreDto = new ColoreDto();
+listaColori: ColoreDto[] = [];
+state = 'ricerca';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void  {
+    const oss: Observable<ColoreDto[]> = this.http.get<ColoreDto[]>(this.urlPath + '/lista-colori');
+    const sub: Subscription = oss.subscribe(risp => {this.listaColori = risp});
   }
+  cerca(): void {
+    const oss: Observable<ColoreDto[]> = this.http.get<ColoreDto[]>(this.urlPath + '/colori-find');
+    const sub: Subscription = oss.subscribe(risp => {this.listaColori = risp});
+  }
+  chiediModifica(colore:ColoreDto): void {
+    this.state = 'modifica';
+    this.coloreForm = Object.assign({},colore);
 
+  }
+  chiediElimina(colore:ColoreDto): void {
+    this.state = 'elimina';
+    this.coloreForm = Object.assign({},colore);
+
+  }
+  nuovo(): void {
+    this.state = 'aggiungi';
+    this.coloreForm.id = 0;
+  }
+  chiudi(): void {
+    this.state = 'ricerca';
+    this.colore = new ColoreDto();
+    this.coloreForm = new ColoreDto();
+  }
+  conferma(): void {
+    let urlEnd: string;
+    switch(this.state){
+      case 'modifica': {
+        urlEnd = '/colori-update';
+        break;
+      }
+      case 'elimina': {
+        urlEnd = '/colori-delete';
+        break;
+      }
+      case 'aggiungi': {
+        urlEnd = '/colori-add';
+        break;
+      }
+      const oss: Observable<ColoreDto[]> = this.http.post<ColoreDto[]>(this.urlPath + urlEnd, this.coloreForm);
+      this.ngOnInit();
+    }
+  }
 }
