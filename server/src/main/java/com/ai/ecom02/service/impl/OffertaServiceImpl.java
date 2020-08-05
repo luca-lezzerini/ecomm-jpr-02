1a8e3aedc5f5fc52c063703aba894a1317afa075
Giu <123giuliogiorgi@gmail.com>
2020-08-05 15:47:40 +0200

    rep e service Offerta
    completati

---
+++ server/src/main/java/com/ai/ecom02/service/impl/OffertaServiceImpl.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ai.ecom02.service.impl;
+
+import com.ai.ecom02.model.Offerta;
+import com.ai.ecom02.repository.RepOfferta;
+import com.ai.ecom02.service.OffertaServiceCrud;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Narga
+ */
+@Service
+public class OffertaServiceImpl implements OffertaServiceCrud {
+    @Autowired 
+    RepOfferta repOfferta ;                                         // stiamo dicendo di autoinstanziare questa casse con tipo+ nome 
+
+    @Override
+    public Offerta add(Offerta offerta) {
+       return  repOfferta.save(offerta);
+        
+    }
+
+    @Override
+    public void delete(Offerta offerta) {
+        repOfferta.delete(offerta);
+    }
+
+    @Override
+    public Offerta update(Offerta offerta) {
+       return repOfferta.save(offerta);
+    }
+
+    @Override
+    public List<Offerta> getAll() {
+       return repOfferta.findAll();
+    }
+
+    @Override
+    public Offerta findById(Offerta offerta) {
+        
+//       Optional<Offerta> o = repOfferta.findById(offerta.getId());
+//       Offerta off= o.get();
+
+        return repOfferta.getOne(offerta.getId());
+    }
+
+    @Override
+    public Offerta findByCodice(Offerta offerta) {
+       return repOfferta.findByCodice(offerta.getCodice());
+    }
+
+    @Override
+    public List<Offerta> findBycodiceLike(Offerta offerta) {
+       return repOfferta.findByCodiceLike(offerta.getCodice());
+    }
+
+    @Override
+    public Offerta FindByDescrizione(Offerta offerta) { 
+       return repOfferta.FindByDescrizione(offerta.getDescrizione());
+    }
+
+    @Override
+    public List<Offerta> FindByDescrizioneLike(Offerta offerta) {
+        
+        return repOfferta.FindByDescrizioneLike(offerta);
+    }
+    
+    
+    
+    
+}
