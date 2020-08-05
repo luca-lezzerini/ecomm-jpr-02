a775ee08964c32eb976100c00c9349a989c3a068
Giu <123giuliogiorgi@gmail.com>
2020-08-05 12:20:05 +0200

    File mancante

---
+++ client-ecomm02/src/app/offerta-crud/offerta-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offerta-crud',
+  templateUrl: './offerta-crud.component.html',
+  styleUrls: ['./offerta-crud.component.css']
+})
+export class OffertaCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
