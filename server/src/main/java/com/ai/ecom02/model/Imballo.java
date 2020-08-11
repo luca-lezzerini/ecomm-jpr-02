2f124999844070d578ae060785bab73a083faffc
DESKTOP-FRANCES\Romanelli <romanelli.france@gmail.com>
2020-08-11 11:32:42 +0200

    Entity settate in IDENTITYinvece che AUTO

--- server/src/main/java/com/ai/ecom02/model/Imballo.java
+++ server/src/main/java/com/ai/ecom02/model/Imballo.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 public class Imballo implements Serializable{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
