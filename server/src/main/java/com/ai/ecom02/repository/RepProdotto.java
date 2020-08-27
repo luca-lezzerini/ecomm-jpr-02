bfb69bb9a1234a3fc27da395ea980df5a2681eb1
LAPTOP-MARCO\rcora <ramello.rco@gmail.com>
2020-08-27 10:08:55 +0200

    Modificato Prodotto
    Modificato Prodotto

--- server/src/main/java/com/ai/ecom02/repository/RepProdotto.java
+++ server/src/main/java/com/ai/ecom02/repository/RepProdotto.java
@@ -3,6 +3,8 @@ package com.ai.ecom02.repository;
 import com.ai.ecom02.model.Prodotto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 //import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.stereotype.Repository;
@@ -20,8 +22,8 @@ public interface RepProdotto extends JpaRepository<Prodotto, Long> {
 
     List<Prodotto> findByPrezzoLike(String prezzo);
 
-  
-    List<Prodotto> findByCodiceLikeOrDescrizioneLike( String codice, String descrizione);
+  @Query("SELECT p FROM Prodotto p WHERE codice LIKE :str OR descrizione LIKE :str ")
+    List<Prodotto> findByCodiceLikeOrDescrizioneLike(@Param("str") String str);
 
    
 
