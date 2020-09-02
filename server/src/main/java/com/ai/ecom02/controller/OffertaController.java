7390e72814102b52f72854879a658850dd0f055e
gianmarcopetrillo <67779659+gianmarcopetrillo@users.noreply.github.com>
2020-09-02 12:01:30 +0200

    Controller offerta ultimato

--- server/src/main/java/com/ai/ecom02/controller/OffertaController.java
+++ server/src/main/java/com/ai/ecom02/controller/OffertaController.java
@@ -5,8 +5,12 @@
  */
 package com.ai.ecom02.controller;
 
+import com.ai.ecom02.dto.ListaOfferteDto;
+import com.ai.ecom02.dto.OffertaDto;
 import com.ai.ecom02.dto.RicercaDto;
 import com.ai.ecom02.model.Offerta;
+import com.ai.ecom02.model.Token;
+import com.ai.ecom02.service.SecurityService;
 import com.ai.ecom02.service.impl.OffertaServiceImpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,50 +27,58 @@ public class OffertaController {
     @Autowired
     OffertaServiceImpl srvOfferta;
 
+    @Autowired
+    SecurityService securityService;
+
     @RequestMapping(value = ("/lista-offerte"))                                // tutte le volte che arriva una chiamata con quel url il controller rispondera con questo metodo                                                        
     @ResponseBody                                                               // la risposta passera direttamente al client invece ad una view nel server 
-    public List<Offerta> listaProdotti() {
-        return srvOfferta.getAll();
+    public ListaOfferteDto listaProdotti(
+            @RequestBody OffertaDto dto
+    ) {
+        Token t = dto.getToken();
+        t = securityService.retrieveToken(t);
+        List<Offerta> lista = srvOfferta.getAll();
+        ListaOfferteDto listaDto = new ListaOfferteDto(lista, t);
+        return listaDto;
     }
 
     @RequestMapping(value = ("/offerta-add"))
     @ResponseBody
     public void addProdotto(
-            @RequestBody Offerta offerta // la richiesta arriva dal application client 
+            @RequestBody OffertaDto offerta // la richiesta arriva dal application client 
     ) {
-        srvOfferta.add(offerta);
+        Token t = offerta.getToken();
+        t = securityService.retrieveToken(t);
+        srvOfferta.add(offerta.getOfferta());
     }
 
     @RequestMapping(value = ("/offerta-delete"))
     @ResponseBody
-    public void deleteOfferta(@RequestBody Offerta offerta) {
-        
-        srvOfferta.delete(offerta);
+    public void deleteOfferta(@RequestBody OffertaDto offerta) {
+        Token t = offerta.getToken();
+        t = securityService.retrieveToken(t);
+        srvOfferta.delete(offerta.getOfferta());
+
     }
 
     @RequestMapping(value = ("/offerta-update"))
     @ResponseBody
-    public void updateOfferta(@RequestBody Offerta offerta) {
+    public void updateOfferta(@RequestBody OffertaDto offerta) {
+        Token t = offerta.getToken();
+        t = securityService.retrieveToken(t);
+        srvOfferta.update(offerta.getOfferta());
+    }
 
-        srvOfferta.update(offerta);
+    @RequestMapping(value = ("/offerta-find"))
+    @ResponseBody
+    public ListaOfferteDto findByCodiceLike(@RequestBody RicercaDto ricerca) {
+
+        Token t = ricerca.getToken();
+        t = securityService.retrieveToken(t);
+        List<Offerta> lista = srvOfferta.findByDescrizioneOrCodiceLike(ricerca);
+        ListaOfferteDto listaDto = new ListaOfferteDto(lista, t);
+        return listaDto;
 
     }
-    
-    
-    
-    @RequestMapping(value=("/offerta-find"))
-    @ResponseBody
-    public List<Offerta> findByCodiceLike(@RequestBody RicercaDto ricerca){
-        
-        return srvOfferta.findByDescrizioneOrCodiceLike(ricerca);
-        
-    
-}
-    
-    
-    
-    
-    
-    
 
 }
