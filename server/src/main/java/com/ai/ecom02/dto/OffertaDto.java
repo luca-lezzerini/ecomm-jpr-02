53a6ba037374e919044ab7451037d79f5209008c
gianmarcopetrillo <67779659+gianmarcopetrillo@users.noreply.github.com>
2020-09-02 11:40:17 +0200

    Aggiustato Offerta dto

--- server/src/main/java/com/ai/ecom02/dto/OffertaDto.java
+++ server/src/main/java/com/ai/ecom02/dto/OffertaDto.java
@@ -5,6 +5,7 @@
  */
 package com.ai.ecom02.dto;
 
+import com.ai.ecom02.model.Offerta;
 import com.ai.ecom02.model.Token;
 
 /**
@@ -13,50 +14,24 @@ import com.ai.ecom02.model.Token;
  */
 public class OffertaDto implements Tokenizabile {
 
-    private String codice;
-
-    private String descrizione;
-
-    private double scontoPercentuale;
+    private Offerta offerta;
 
     private Token token;
 
     public OffertaDto() {
     }
 
-    public OffertaDto(String codice, String descrizione, double scontoPercentuale) {
-        this.codice = codice;
-        this.descrizione = descrizione;
-        this.scontoPercentuale = scontoPercentuale;
+    public OffertaDto(Offerta offerta, Token token) {
+        this.offerta = offerta;
+        this.token = token;
     }
 
-    public String getCodice() {
-        return codice;
+    public Offerta getOfferta() {
+        return offerta;
     }
 
-    public void setCodice(String codice) {
-        this.codice = codice;
-    }
-
-    public String getDescrizione() {
-        return descrizione;
-    }
-
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
-    }
-
-    public double getScontoPercentuale() {
-        return scontoPercentuale;
-    }
-
-    public void setScontoPercentuale(double scontoPercentuale) {
-        this.scontoPercentuale = scontoPercentuale;
-    }
-
-    @Override
-    public String toString() {
-        return "OffertaDto{" + "codice=" + codice + ", descrizione=" + descrizione + ", scontoPercentuale=" + scontoPercentuale + ", token=" + token + '}';
+    public void setOfferta(Offerta offerta) {
+        this.offerta = offerta;
     }
 
     @Override
