1a8db9682a517533a15799ee5aff3babc817c6ba
LAPTOP-H5TDA92K\giuse <g.migliaccio@accademiainformatica.com>
2020-09-10 12:04:24 +0200

    associa-taglia aggiornamento
    aggiornamento

--- client-ecomm02/src/app/Associazioni/associa-taglia/associa-taglia.component.ts
+++ client-ecomm02/src/app/Associazioni/associa-taglia/associa-taglia.component.ts
@@ -1,4 +1,9 @@
+import { Prodotto } from './../../model/prodotto';
+import { Router } from '@angular/router';
+import { ProdottoService } from './../../prodotto.service';
+import { TagliaServiceService } from './../../service/taglia-service.service';
 import { Component, OnInit } from '@angular/core';
+import { RicercaDto } from 'src/app/dto/ricerca-dto';
 
 @Component({
   selector: 'app-associa-taglia',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssociaTagliaComponent implements OnInit {
 
-  constructor() { }
+  ricerca: RicercaDto = new RicercaDto();
+  state = false;
+
+
+
+
+  constructor(public memT : TagliaServiceService, public memP : ProdottoService, private router: Router) { }
 
   ngOnInit() {
+    this.memP.lista();
+  }
+  
+  seleziona(prodotto: Prodotto): void {
+    this.state = true;
+    this.memP.prodottoForm = Object.assign({}, prodotto); 
   }
 
+  cercaProdotto() {
+    this.memP.cerca(this.ricerca);
+  }
 }
