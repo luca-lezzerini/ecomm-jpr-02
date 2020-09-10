d37143bc98941b804d8404d91477779e277f7c86
DESKTOP-FRANCES\Romanelli <romanelli.france@gmail.com>
2020-09-10 11:13:22 +0200

    #100

--- client-ecomm02/src/app/Associazioni/associa-imballo/associa-imballo.component.ts
+++ client-ecomm02/src/app/Associazioni/associa-imballo/associa-imballo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdottoService } from 'src/app/prodotto.service';
+import { ImballoServiceService } from 'src/app/service/imballo-service.service';
+import { Router } from '@angular/router';
+import { RicercaDto } from 'src/app/dto/ricerca-dto';
+import { Prodotto } from 'src/app/model/prodotto';
 
 @Component({
   selector: 'app-associa-imballo',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssociaImballoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private srvProdotto: ProdottoService, private mem: ImballoServiceService) { }
 
   ngOnInit(): void {
+    this.srvProdotto.lista();
   }
 
+  ricerca: RicercaDto = new RicercaDto();
+  state = 'ricerca';
+
+  cercaProdotto() {
+    this.srvProdotto.cerca(this.ricerca);
+  }
+
+  visualizza(prodotto: Prodotto): void {
+    this.srvProdotto.prodottoForm = Object.assign({}, prodotto);
+    this.state = 'visualizza';
+  }
 }
