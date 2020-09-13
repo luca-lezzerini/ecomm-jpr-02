b08835b5b9d2e46cc43591cbc897884f3b31a049
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2020-09-13 08:09:13 +0200

    Generazione colori

--- server/src/main/java/com/ai/ecom02/controller/TestDataGeneratorController.java
+++ server/src/main/java/com/ai/ecom02/controller/TestDataGeneratorController.java
@@ -1,7 +1,9 @@
 package com.ai.ecom02.controller;
 
 import com.ai.ecom02.model.Categoria;
+import com.ai.ecom02.model.Colore;
 import com.ai.ecom02.repository.RepCategoria;
+import com.ai.ecom02.repository.RepColore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ public class TestDataGeneratorController {
 
     @Autowired
     RepCategoria repCategoria;
+    @Autowired
+    RepColore repColore;
 
     @RequestMapping("/genera-dati")
     public void generaDati() {
@@ -23,6 +27,13 @@ public class TestDataGeneratorController {
         for (int i = 0; i < 1000; i++) {
             Categoria cx = new Categoria(null, "Categoria n. " + i);
             cx = repCategoria.save(cx);
+        }
+
+        repColore.deleteAllInBatch();
+
+        for (int i = 0; i < 10_000; i++) {
+            Colore cx = new Colore(null, "Colore n. " + i);
+            cx = repColore.save(cx);
         }
     }
 
